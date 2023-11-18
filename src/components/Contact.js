bd65e5f1bdb30f610d2b39d0765cb50ed718d3e5
kodecorp-FC360 <contactus@kodecorp.com>
2023-11-18 19:18:53 +0530

    whatsapp

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,9 +6,9 @@ const Contact = () => {
 <footer>
 		<div className="container center-text">
 			<ul className="social-icons">
-				<li><a href="https://www.facebook.com/2Manoj1" target="_blank" rel="noopener noreferrer"><i className="icon icon-facebook"></i></a></li>
-				<li><a href="https://www.twitter.com/ManojMukherje19" target="_blank" rel="noopener noreferrer"><i className="icon icon-twitter"></i></a></li>
-				<li><a href="https://wa.me/918892525120" target="_blank" rel="noopener noreferrer"><img alt="WP" src={wp} /></a></li>
+				{/* <li><a href="https://www.facebook.com/2Manoj1" target="_blank" rel="noopener noreferrer"><i className="icon icon-facebook"></i></a></li>
+				<li><a href="https://www.twitter.com/ManojMukherje19" target="_blank" rel="noopener noreferrer"><i className="icon icon-twitter"></i></a></li> */}
+				<li><a href="https://wa.me/9052335555" target="_blank" rel="noopener noreferrer"><img alt="WP" src={wp} /></a></li>
 			</ul>
 		</div>
 	</footer>
